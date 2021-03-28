4b3ff68520c85a3decf41e5c43b507bb76a1f403
Guilherme Felipe do Nascimento Reis <guifeliper@gmail.com>
2021-03-28 16:55:47 +0200

    feat: pokemon name list

--- src/pages/pokedex/index.page.tsx
+++ src/pages/pokedex/index.page.tsx
@@ -1,9 +1,28 @@
 import React from "react";
+import Link from "next/link";
 
-// import { Container } from './styles';
+import { IPokemon, PokedexProps } from "./types";
 
-const Pokedex: React.FC = () => {
-  return <div>Pokedex</div>;
+export const getStaticProps = async () => {
+  const res = await fetch("https://pokeapi.co/api/v2/pokemon");
+  const data = await res.json();
+
+  return {
+    props: { pokemons: data.results },
+  };
+};
+const Pokedex: React.FC<PokedexProps> = ({ pokemons }) => {
+  return (
+    <div>
+      <h1>All Pokemons</h1>
+      {pokemons &&
+        pokemons.map((pokemon: IPokemon) => (
+          <div>
+            <h3>{pokemon.name}</h3>
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default Pokedex;
